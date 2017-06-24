8fad8a4f1591fb4a7d7d1bdf932c5918197b475c
Michael F. Ellis <michael.f.ellis@gmail.com>
2017-06-24 09:08:06 -0400

    Fix <style> rendering under Transcrypt.
    The hasattr test in renderCss() was failing when it shouldn't have.
    Fixed by removal. Updated tests/client.py to create and append a style
    element to detect problems related to Style() on the client side.

--- tests/client.py
+++ tests/client.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Description: Client side of sanity check
+Uses JS functions insertAdjacentHTML, innerHTML and addEventListener.
+See https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+    https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+    https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
 Author: Mike Ellis
 Copyright 2017 Owner
 """
 from htmltree import *
 def start():
     console.log("Starting")
-    newcontent = H1("Sanity check PASS", _class='test', style=dict(color='green'))
-    console.log(newcontent.render(0))
+    ## insert a style element at the end of the <head?
+    cssrules = {'.test':{'color':'green', 'text-align':'center'}}
+    style = Style(**cssrules)
+    document.head.insertAdjacentHTML('beforeend', style.render())
+
+    ## Replace the <body> content
+    newcontent = Div(H1("Sanity check PASS", _class='test'))
     document.body.innerHTML = newcontent.render()
     console.log("Finished")
+
+## JS is event driven.
+## Wait for DOM load to complete before firing
+## our start() function.
 document.addEventListener('DOMContentLoaded', start)
 
