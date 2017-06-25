7906a12582dbf8c97320d0497e25b2162b3b566c
Michael F. Ellis <michael.f.ellis@gmail.com>
2017-06-25 12:09:02 -0400

    add setup files for PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'htmltree',
+  packages = ['htmltree'], # this must be the same as the name above
+  version = '0.7.0',
+  description = 'Generalized nested html element tree with recursive rendering',
+  author = 'Michael F. Ellis',
+  author_email = 'michael.f.ellis@gmail.com',
+  url = 'https://github.com/Michael-F-Ellis/htmltree', # use the URL to the github repo
+  download_url = 'https://github.com/Michael-F-Ellis/htmltree/archive/0.7.0.tar.gz',
+  keywords = ['html', 'css', 'Transcrypt', 'web app development'],
+  classifiers = [],
+)
