b8fe4a46ab0f2344c1f8cae81ab2ce84e0c6a497
Michael F. Ellis <michael.f.ellis@gmail.com>
2017-06-27 14:01:46 -0400

    Prep to push 0.7.2 to PyPI

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'htmltree',
   packages = ['htmltree'], # this must be the same as the name above
-  version = '0.7.0',
+  version = '0.7.2',
   description = 'Generalized nested html element tree with recursive rendering',
   author = 'Michael F. Ellis',
   author_email = 'michael.f.ellis@gmail.com',
   url = 'https://github.com/Michael-F-Ellis/htmltree', # use the URL to the github repo
-  download_url = 'https://github.com/Michael-F-Ellis/htmltree/archive/0.7.0.tar.gz',
+  download_url = 'https://github.com/Michael-F-Ellis/htmltree/archive/0.7.2.tar.gz',
   keywords = ['html', 'css', 'Transcrypt', 'web app development'],
   classifiers = [],
 )
