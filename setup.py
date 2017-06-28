83113d891e57d8b9dc9e8f6c8342abaa54168fdb
Michael F. Ellis <michael.f.ellis@gmail.com>
2017-06-28 15:21:06 -0400

    update for 0.7.3 release

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+with open('README.md') as file:
+    long_description = file.read()
 setup(
   name = 'htmltree',
   packages = ['htmltree'], # this must be the same as the name above
-  version = '0.7.2',
+  version = '0.7.3',
   description = 'Generalized nested html element tree with recursive rendering',
+  long_description = long_description,
   author = 'Michael F. Ellis',
   author_email = 'michael.f.ellis@gmail.com',
   url = 'https://github.com/Michael-F-Ellis/htmltree', # use the URL to the github repo
-  download_url = 'https://github.com/Michael-F-Ellis/htmltree/archive/0.7.2.tar.gz',
+  download_url = 'https://github.com/Michael-F-Ellis/htmltree/archive/0.7.3.tar.gz',
   keywords = ['html', 'css', 'Transcrypt', 'web app development'],
   classifiers = [],
 )
